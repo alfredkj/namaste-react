0ccc7719385a137269ad53c76eb1d9ee5325ea3f
alfred johnson k <alfredk.j@gmail.com>
2023-03-25 11:04:56 +0800

    Added index.css and imported in the html

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
-        const  h1Heading = React.createElement("h1",{},"Hello World From Namaste React !");
-        const root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(h1Heading);
+const heading = React.createElement(
+  "h1",
+  { id: "heading"},
+  "Hello World From Namaste React !"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
