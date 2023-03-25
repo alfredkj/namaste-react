aa32067eae25e30a4e0dc9ef29c1da6fe8cf5bab
alfred johnson k <alfredk.j@gmail.com>
2023-03-25 11:26:25 +0800

    Adding nested elements in  React

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 const heading = React.createElement(
   "h1",
-  { id: "heading"},
+  {},
   "Hello World From Namaste React !"
 );
+const child = React.createElement("div", { id: "child" }, heading);
+const parent = React.createElement("div", { id: "parent" }, child);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
