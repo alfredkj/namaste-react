9f05fcbea979660256f4d6dd624e0a1903ad752d
alfred johnson k <alfredk.j@gmail.com>
2023-03-25 10:07:11 +0800

    createElement,createRoot & render in React

---
+++ App.js
@@ -0,0 +1,3 @@
+        const  h1Heading = React.createElement("h1",{},"Hello World From Namaste React !");
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(h1Heading);
