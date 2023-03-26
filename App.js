63139c95fdead1b7172f8c41127eeda70f93c43a
alfredkj <alfredk.j@gmail.com>
2023-03-26 18:43:41 +0800

    E02 : Parcel bundler, package & package-lock

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
-const heading = React.createElement(
+//The "react" means the react in node modules.
+import React from "react";
+import ReactDom from "react-dom";
+
+const heading1 = React.createElement(
   "h1",
   {},
-  "Hello World From Namaste React !"
+  "Hi I'm Alfred :D"
 );
-const child = React.createElement("div", { id: "child" }, heading);
-const parent = React.createElement("div", { id: "parent" }, child);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const heading2 = React.createElement(
+  "h2",
+  {},
+  "This is fun to do!"
+);
+const heading3 = React.createElement(
+  "h3",
+  {},
+  "This is nice to watch !"
+);
+const heading4 = React.createElement(
+  "h4",
+  {},
+  "How long will it take to master it ?"
+);
+const child1 = React.createElement("div", { id: "child1" }, heading1);
+const child2 = React.createElement("div", { id: "child2" }, heading2);
+const child3 = React.createElement("div", { id: "child3" }, heading3);
+const child4 = React.createElement("div", { id: "child4" }, heading4);
+const parent = React.createElement("div", { id: "parent" }, [child1,child2,child3,child4]);
+console.log(parent);
+const branch = ReactDom.createRoot(document.getElementById("branch"));
+branch.render(parent);
